0b831722c6fd4cf33e7fd12add6c70621763ae08
DaveedGangi <daveedgangi@gmail.com>
2025-02-26 14:30:59 +0530

    addedtodoapp

--- src/components/work/index.js
+++ src/components/work/index.js
@@ -21,6 +21,15 @@ class Work extends Component {
         <div className="work-container">
 
         <div className="card">
+                    <img className="card-img-top" src="https://imgbb.io/ib/cwmCgey9QB6cW5h_1740560091.png" alt="not-found"/>
+                    <div className="card-body">
+                        <h5 className="card-title">Todo App</h5>
+                        <p className="card-text">To-Do App allows you to efficiently manage your tasks with ease.All task data is stored locally on your device, ensuring your information is always accessible and secure. Stay organized and boost your productivity with user-friendly To-Do App. </p>
+                        <a href="https://todoappreactdaveed.onrender.com" target="_blank" rel="noreferrer" class="btn btn-primary">Go Simple Calculator<i class="fa-solid fa-arrow-right"></i></a>
+                    </div>
+            </div>
+
+        <div className="card">
                     <img className="card-img-top" src="https://i.ibb.co/tp5tZT5D/Screenshot-2025-02-14-182419.png" alt="not-found"/>
                     <div className="card-body">
                         <h5 className="card-title">Simple Calculator</h5>
