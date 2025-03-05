8960b045525e68228742d1277ca1fd3f519a5bb6
DaveedGangi <daveedgangi@gmail.com>
2025-03-05 17:55:01 +0530

    addedchatbot

--- src/App.js
+++ src/App.js
@@ -20,6 +20,12 @@ class App extends Component {
     setTimeout(() => {
       this.setState({ showAnimation: false });
     }, 4000); // Change duration as needed
+
+        // Load the Dialogflow Messenger script dynamically
+        const script = document.createElement("script");
+        script.src = "https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1";
+        script.async = true;
+        document.body.appendChild(script);
   }
 
   render() {
@@ -35,6 +41,15 @@ class App extends Component {
               <Route exact path="/" component={Home} />
               <Route exact path="/work" component={Work} />
             </Switch>
+
+          {/* Dialogflow Messenger Component */}
+          <df-messenger
+              chat-icon="https://img.freepik.com/free-vector/graident-ai-robot-vectorart_78370-4114.jpg?w=740&st=1718261464~exp=1718262064~hmac=ff5527dee568fee0a0bfbff2c2582f6f91e7d184c0391f70295e31d688b8705b"
+              intent="WELCOME"
+              chat-title="DaveedBot"
+              agent-id="7dbc5cd0-9536-48da-bd38-7bd988282d91"
+              language-code="en"
+            ></df-messenger>
 
             <footer className="footer">
               <div className="footer-content">
