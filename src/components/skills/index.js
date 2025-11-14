b8533fdfe23242a38df56f0afe49c769d92b92fd
Daveed Gangi <daveedgangi@gmail.com>
2025-11-14 09:30:29 +0530

    added git skill

--- src/components/skills/index.js
+++ src/components/skills/index.js
@@ -3,19 +3,35 @@ import './index.css';
 
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
- const skills = [
-    { name: "HTML", icon: "fab fa-html5", link: "https://developer.mozilla.org/en-US/docs/Web/HTML" },
-    { name: "CSS", icon: "fab fa-css3-alt", link: "https://developer.mozilla.org/en-US/docs/Web/CSS" },
-    { name: "Bootstrap", icon: "fab fa-bootstrap", link: "https://getbootstrap.com/docs/" },
-    { name: "JavaScript", icon: "fab fa-js", link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript" },
-    { name: "React", icon: "fab fa-react", link: "https://reactjs.org/docs/getting-started.html" },
-    { name: "Node.js", icon: "fab fa-node", link: "https://nodejs.org/en/docs" },
-    { name: "Python", icon: "fab fa-python", link: "https://docs.python.org/3/" },
-    { name: "FastAPI", icon: "fas fa-bolt", link: "https://fastapi.tiangolo.com/" },
-    { name: "C#", icon: "fas fa-code", link: "https://learn.microsoft.com/en-us/dotnet/csharp/" },
-    { name: "SQL", icon: "fas fa-database", link: "https://www.w3schools.com/sql/" },
-    { name: "MongoDB", icon: "fab fa-envira", link: "https://www.mongodb.com/docs/" }
-  ];
+ 
+
+
+  const skills = [
+  // Core Web Development
+  { name: "HTML", icon: "fab fa-html5", link: "https://developer.mozilla.org/en-US/docs/Web/HTML" },
+  { name: "CSS", icon: "fab fa-css3-alt", link: "https://developer.mozilla.org/en-US/docs/Web/CSS" },
+  { name: "Bootstrap", icon: "fab fa-bootstrap", link: "https://getbootstrap.com/docs/" },
+  { name: "JavaScript", icon: "fab fa-js", link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript" },
+
+  // Frontend Framework
+  { name: "React", icon: "fab fa-react", link: "https://reactjs.org/docs/getting-started.html" },
+
+  // Backend Development
+  { name: "Node.js", icon: "fab fa-node", link: "https://nodejs.org/en/docs" },
+  { name: "FastAPI", icon: "fas fa-bolt", link: "https://fastapi.tiangolo.com/" },
+  { name: "Python", icon: "fab fa-python", link: "https://docs.python.org/3/" },
+
+  // Databases
+  { name: "SQL", icon: "fas fa-database", link: "https://www.w3schools.com/sql/" },
+  { name: "MongoDB", icon: "fab fa-envira", link: "https://www.mongodb.com/docs/" },
+
+  // Dev Tools
+  { name: "Git", icon: "fab fa-git-alt", link: "https://git-scm.com/doc" },
+
+  // Additional
+  { name: "C#", icon: "fas fa-code", link: "https://learn.microsoft.com/en-us/dotnet/csharp/" }
+];
+
 
 const Skills = () => {
   return (
